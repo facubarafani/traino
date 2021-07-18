7581fae8efbd09206baee6aedccf4855683ba363
Manuel-Bobadilla <manuelcba@outlook.com>
2021-07-18 19:59:05 -0300

    create relation user-membership

--- api/controllers/MemberShipController.js
+++ api/controllers/MemberShipController.js
@@ -1,22 +1,20 @@
-const Membership = require("../models/Membership");
-const UserController = require("./UserController");
+
 
 module.exports = {
-    createMembership: async function (req, res){
+    create: async function (req, res){
       const cost = req.param('cost');
       const initialDate = req.param('initialDate');
       const duration = req.param('duration');
       const fechaVencimiento = req.param('fechaVencimiento');
       const userId = req.param('userId');
-      var createdMembership = await Membership.create({cost: cost, initialDate: initialDate, duration: duration, fechaVencimiento: fechaVencimiento, user: 1}).fetch();
-      // await Membership.populate(userId, 'user')
-      await User.findOne({id: userId}).update({membership: createdMembership.id})
 
+      const createdMembership = await Membership.create({cost: cost, initialDate: initialDate, duration: duration, fechaVencimiento: fechaVencimiento, user: userId}).fetch();
+
+      await User.update({id: userId}).set({membership: createdMembership.id});
       if (createdMembership) {
         res.redirect('/');
       } else {
         console.log('Error al crear membresia');
       }
-  
     },
   };
